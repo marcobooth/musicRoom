2e5b57f11acb00e9271e1e4198d34754fad3fcbe
Marco BOOTH <mbooth@e3r4p14.42.fr>
2017-06-11 13:17:19 +0200

    first set of changes

--- musicRoom/AppDelegate.swift
+++ musicRoom/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
@@ -39,6 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let handled: Bool = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication:sourceApplication, annotation: annotation)
+        // Add any custom logic here.
+        return handled
     }
 
 
--- musicRoom/ViewController.swift
+++ musicRoom/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func signupAction(_ sender: UIButton) {
+        FIRAuth.auth()?.createUser(withEmail: login.text!, password: password.text!) { user, error in
+
+            if error == nil {
+                user?.sendEmailVerification(completion: nil)
+            } else {
+                print("there was an error", error)
+            }
+        
+        }
+        print(login.text, password.text)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginAction(_ sender: UIButton) {
+        FIRAuth.auth()?.signIn(withEmail: login.text!, password: password.text!) { user, error in
+            if error == nil && user?.isEmailVerified == true {
+                print("no error")
+            } else {
+                print("error", error)
+            }
+            print("email verified", user?.isEmailVerified)
+        }
     }
 
-
+    @IBAction func loginWithFacebook(_ sender: Any) {
+        let login = FBSDKLoginManager()
+        login.logIn(withReadPermissions: ["public_profile"], from: self) { (result, error) in
+            print(result)
+            print(error)
+            let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            print(credential)
+            FIRAuth.auth()?.signIn(with: credential) { thing in
+                print("account should have been created")
+            }
+        }
+    }
 }
+
+
+//        let loginButton = FBSDKLoginButton()
+//        loginButton.delegate = self
+//        loginButton.center = view.center
+//        view.addSubview(loginButton)
+//    /**
+//     Sent to the delegate when the button was used to login.
+//     - Parameter loginButton: the sender
+//     - Parameter result: The results of the login
+//     - Parameter error: The error (if any) from the login
+//     */
+//    public func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error?) {
+//        print(result)
+////      print("error", error)
+//        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+//        print(credential)
+//        FIRAuth.auth()?.signIn(with: credential) { thing in
+//            print("account should have been created")
+//        }
+//    }
+//
+//
+//    /**
+//     Sent to the delegate when the button was used to logout.
+//     - Parameter loginButton: The button that was clicked.
+//     */
+//    public func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+//        print("apparently I'm now logging out")
+//    }
 
