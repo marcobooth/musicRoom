2dca7507ebdc07e64d488e57bf27527dc2bb0b45
Marco BOOTH <mbooth@e1r8p9.42.fr>
2017-06-13 15:37:40 +0200

    deezer now works

--- musicRoom/DeezerSession.swift
+++ musicRoom/DeezerSession.swift
@@ -14,6 +14,7 @@ class DeezerSession : NSObject, DeezerSessionDelegate, DZRPlayerDelegate {
     
     var deezerConnect : DeezerConnect?
     var player: DZRPlayer?
+    var currentUser: DZRUser?
     
 //    let DEEZER_TOKEN_KEY = "DeezerTokenKey"
 //    let DEEZER_EXPIRATION_DATE_KEY = "DeezerExpirationDateKey"
@@ -32,13 +33,37 @@ class DeezerSession : NSObject, DeezerSessionDelegate, DZRPlayerDelegate {
     }
     
     func deezerDidLogin() {
+//        print("currentUser", self.currentUser)
         print("did login")
-        print("\n")
+        DZRUser.object(withIdentifier: "me", requestManager:DZRRequestManager.default(), callback: {(_ objs: Any?, _ error: Error?) -> Void in
+            if let user = objs as? DZRUser {
+                self.currentUser = user
+//                print("currentUser", self.currentUser)
+            }
+        })
 //        print(self.deezerConnect?.accessToken)
 //        print(self.deezerConnect?.userId)
 //        print(self.deezerConnect?.expirationDate)
     }
     
+    func search() {
+        //        DZRTrack.object(withIdentifier: "3135556", requestManager: DZRRequestManager.default(), callback: {(_ objs: Any?, _ error: Error?) -> Void in
+        //                print("im in this thing")
+        //                objectList.add
+        //                DeezerSession.sharedInstance.player?.play(objs as! DZRPlayable!, at: 0)
+        //        })
+        //        DZRObject.search(for: DZRSearchType.track, withQuery: "californication", requestManager: DZRRequestManager.default(), callback: {(_ results: DZRObjectList?, _ error: Error?) -> Void in
+        //            results!.allObjects(with: DZRRequestManager.default(), callback: {(_ objs: [Any]?, _ error: Error?) -> Void in
+        //                print(objs?[0])
+        //                print(type(of: objs?[0]))
+        //                DeezerSession.sharedInstance.player?.play(objs as! DZRPlayable!, at: 0)
+        //                //                for obj in objs! {
+        //                //                    print(obj)
+        //                //                }
+        //            })
+        //        })
+    }
+
     func deezerDidLogout() {
         print("did logout")
     }
