925471165f75af6f8517bb5a10791ea7f709e789
Teo Fleming <mokolodi1@gmail.com>
2017-08-10 11:48:11 +0200

    minimize Firebase logs

--- musicRoom/AppDelegate.swift
+++ musicRoom/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
         
         // Facebook login stuff
